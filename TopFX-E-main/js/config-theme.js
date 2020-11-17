50d304264b3b1f126635a6784735097e5d1a6ee4
Tee15 <temitopedml@gmail.com>
2020-11-17 07:22:57 +0100

    initial commit

---
+++ TopFX-E-main/js/config-theme.js
@@ -0,0 +1,35 @@
+
+'use strict';
+
+const HomepageApp = {
+    //----------- 1. uikit slideshow -----------
+    theme_slideshow: function() {
+        UIkit.slideshow('.in-slideshow', {
+            autoplay: true,
+            autoplayInterval: 7000,
+            pauseOnHover: false,
+            animation: 'slide',
+            minHeight: 342,
+            maxHeight: 542
+        });
+    },
+    //---------- 2. Counter config -----------
+    theme_counter: function() {
+        const counter = new counterUp({
+            selector: '.count',
+            start: 0,
+            duration: 3200,
+            intvalues: true,
+            interval: 50
+        });
+        counter.start();
+    },
+    theme_init: function() {
+        HomepageApp.theme_slideshow();
+        HomepageApp.theme_counter();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    HomepageApp.theme_init();
+});
